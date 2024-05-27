05a9fa0b4f5bfef89bfe7aed1063e8126b836ad5
alannguyen127 <huannghiem2711@gmail.com>
2024-05-27 14:30:40 +0700

    edit App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Mai Thy!</h1>
+      <button onClick={() => alert("I love you")}>
+        Click here to receive message from Nghiem
+      </button>
+    </div>
+  );
+}
+
+export default App;
