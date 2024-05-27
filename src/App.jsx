f0152728f59c67899b2f6156a766c4580c19c40d
alannguyen127 <huannghiem2711@gmail.com>
2024-05-27 14:58:34 +0700

    add image and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 
 function App() {
+  function handleClick() {
+    alert("I love you 💋 💋 💋");
+    document.body.style.background =
+      "url(../public/seamless-pattern-small-hearts-background-valentine-s-day_578229-335.avif)";
+  }
+
   return (
     <div>
       <h1>Hello Mai Thy!</h1>
-      <button onClick={() => alert("I love you")}>
+      <button onClick={handleClick}>
         Click here to receive message from Nghiem
       </button>
     </div>
